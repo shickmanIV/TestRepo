a025e18155055e859a38c58466b40de719fbdd89
shickmanIV <113073744+shickmanIV@users.noreply.github.com>
2023-04-25 15:43:45 -0700

    Created a tester class, and modified main() for testing

--- testProject/main.cpp
+++ testProject/main.cpp
@@ -1,10 +1,14 @@
 #include <SFML/Graphics.hpp>
 #include "GraphicsChess.hpp"
+#include "MiniGameTest.hpp"
 
 int main()
 {
     GraphicsChess test;
-    test.game();
+    //test.game();
 
+    MiniGameTest testGame;
+    testGame.testOpenWindow();
+    
     return 0;
 }
