217d309b50e46e27c0276b323b9e679142bfaf07
shickmanIV <113073744+shickmanIV@users.noreply.github.com>
2023-07-26 18:37:06 -0700

    Added a temp bool for testing the minigame alone.

--- testProject/main.cpp
+++ testProject/main.cpp
@@ -1,18 +1,32 @@
 #include <SFML/Graphics.hpp>
 #include "Test.hpp"
+#include "MiniGameTest.hpp"
+
+bool TEST_MINIGAME = true;
 
 int main()
 {
-    Test test;
+    if (!TEST_MINIGAME)
+    {
+        Test test;
 
-    test.testMove();
-    test.testCapture();
-    test.testEnPassant();
-    test.testCastle();
-    test.testPromotion();
+        test.testMove();
+        test.testCapture();
+        test.testEnPassant();
+        test.testCastle();
+        test.testPromotion();
 
-    GraphicsChess chess;
-    chess.game();
+        GraphicsChess chess;
+        chess.game();
+    }
+    else //Runs only the minigame.
+    {
+        MiniGameTest mgTester;
+        mgTester.testOpenWindow();
+    }
+    
+
+
     
     return 0;
 }
